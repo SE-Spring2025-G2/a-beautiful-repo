11dedcf721ce84c01e5d1a91e8314071fb97df44
Ayush Pathak <31178268+Raphaeal19@users.noreply.github.com>
2025-01-22 22:03:21 +0000

    Added tests for more coverage

--- tests/test_myfile.py
+++ tests/test_myfile.py
@@ -1,9 +1,15 @@
 from src.myfile import Fibonacci
-
+import pytest
 
 def test_passing() -> None:
     assert Fibonacci(9) == 34
 
+def test_0_iput() -> None:
+    assert Fibonacci(0) == 0
+
+def test_negative_input() -> None:
+    with pytest.raises(ValueError):
+        Fibonacci(-5)
 
 def test_failing() -> None:
     assert Fibonacci(10) == 55
